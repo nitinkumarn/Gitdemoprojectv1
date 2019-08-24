a68ee4500a976472d16b36d473cd3549a6ffeef1
Admin <Admin@Admin-PC>
2019-08-25 00:12:26 +0530

    Committing test2 to branch x

---
+++ src/sample/Test2.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Test2 {
+	
+
+		public static void main(String[] args) {
+		
+			for (int i = 1; i <= 4; i++) {
+				char no='a';
+
+				for (int j = 1; j <=4; j++) {
+					System.out.print(no);
+					no++;
+				}
+				System.out.println();
+				
+			}
+		}
+}
